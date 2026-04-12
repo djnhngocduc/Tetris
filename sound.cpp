9fc1bd5426d2c8da04b47bc68b60f3ebde882e1c
Mangyeuem <143802129+MINGKK2@users.noreply.github.com>
2026-04-12 20:05:06 +0700

    them sound.cpp

---
+++ sound.cpp
@@ -0,0 +1,32 @@
+#include "sound.h"
+#include <iostream>
+
+using namespace std;
+
+void sound::Audio_long(const char* song) {
+	Mix_Music* music = NULL;
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) == -1) {
+		cout << Mix_GetError();
+	}
+	music = Mix_LoadMUS(song);
+	if (music == NULL) {
+		cout << Mix_GetError();
+	}
+	if (!Mix_PlayingMusic()) {
+		Mix_PlayMusic(music, -1);
+	}
+}
+//////////////////////////
+void sound::Audio_short(const char* song) {
+	Mix_Chunk* inlist = NULL;
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) == -1) {
+		cout << Mix_GetError();
+	}
+	inlist = Mix_LoadWAV(song);
+	if (inlist == NULL) {
+		cout << Mix_GetError();
+	}
+	if (!Mix_Playing(-1)) {
+		Mix_PlayChannel(-1, inlist, 0);
+	}
+}
